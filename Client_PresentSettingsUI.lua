0bc4cc9f9a8a71b420056cf8b0cf4fe2ff0b9624
Derfellios <joris.nieuwveld@gmail.com>
2019-01-16 20:46:43 +0100

    Update Client_PresentSettingsUI.lua

--- Client_PresentSettingsUI.lua
+++ Client_PresentSettingsUI.lua
@@ -1,4 +1,5 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	UI.CreateLabel(vert).SetText('Allow transfering: ' .. tostring(Mod.Settings.allowTransfering));
+	UI.CreateLabel(vert).SetText('Reconnaisance cards spawn a commander: ' .. tostring(Mod.Settings.allowRecon));
 end
