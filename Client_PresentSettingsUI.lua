55350a78a7ca3ccd94b9bd8462be9dcc4738525f
Derfellios <joris.nieuwveld@gmail.com>
2019-01-18 16:45:39 +0100

    Update Client_PresentSettingsUI.lua

--- Client_PresentSettingsUI.lua
+++ Client_PresentSettingsUI.lua
@@ -1,5 +1,6 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	UI.CreateLabel(vert).SetText('All starts get a commander: ' .. tostring(Mod.Settings.startWithCommander));
 	UI.CreateLabel(vert).SetText('Allow transfering: ' .. tostring(Mod.Settings.allowTransfering));
-	UI.CreateLabel(vert).SetText('Reconnaisance cards spawn a commander: ' .. tostring(Mod.Settings.allowRecon));
+	UI.CreateLabel(vert).SetText('Reconnaisance cards spawn a commander: ' .. tostring(Mod.Settings.allowRecon));	
 end
