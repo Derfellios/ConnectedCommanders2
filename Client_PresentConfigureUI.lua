56e6bedcd4434abbbb6ca668075298921ce3237f
Derfellios <joris.nieuwveld@gmail.com>
2019-01-18 16:44:03 +0100

    Update Client_PresentConfigureUI.lua

--- Client_PresentConfigureUI.lua
+++ Client_PresentConfigureUI.lua
@@ -1,6 +1,7 @@
 
 function Client_PresentConfigureUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	startWithCommander = UI.CreateCheckBox(vert).SetText('All starting territories receive a commander').SetIsChecked(true);
 	allowTransferingBox = UI.CreateCheckBox(vert).SetText('Allows that a commander can transfer').SetIsChecked(true);
 	allowReconBox = UI.CreateCheckBox(vert).SetText('Reconnaissance cards spawn a commander at a territory').SetIsChecked(true);
 end
