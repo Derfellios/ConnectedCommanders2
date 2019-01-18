b6319ab9cc605fab87b7d555e1822beff9a65ead
Derfellios <joris.nieuwveld@gmail.com>
2019-01-18 16:46:52 +0100

    Update Client_SaveConfigureUI.lua

--- Client_SaveConfigureUI.lua
+++ Client_SaveConfigureUI.lua
@@ -1,4 +1,5 @@
 function Client_SaveConfigureUI(alert)
+	Mod.Settings.startWithCommander = startWithCommanderBox.GetIsChecked()
 	Mod.Settings.allowTransfering = allowTransferingBox.GetIsChecked();
 	Mod.Settings.allowRecon = allowReconBox.GetIsChecked();
 end
