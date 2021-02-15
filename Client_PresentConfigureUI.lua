606c821caf5c173677fae27c9b56f2a4eeb32cb1
Derfellios <joris.nieuwveld@gmail.com>
2021-02-15 20:23:02 +0100

    Update Client_PresentConfigureUI.lua

--- Client_PresentConfigureUI.lua
+++ Client_PresentConfigureUI.lua
@@ -1,8 +1,17 @@
-
 function Client_PresentConfigureUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-	startWithCommanderBox = UI.CreateCheckBox(vert).SetText('All starting territories receive a commander').SetIsChecked(true);
-	allowTransferingBox = UI.CreateCheckBox(vert).SetText('Allows that a commander can transfer').SetIsChecked(true);
-	allowAttackingBox = UI.CreateCheckBox(vert).SetText('Allows that a commander can attack/transfer').SetIsChecked(false);
-	allowReconBox = UI.CreateCheckBox(vert).SetText('Reconnaissance cards spawn a commander on a territory').SetIsChecked(true);
+	local startWithCommander = Mod.Settings.startWithCommander;
+	local allowTransfering = Mod.Settings.allowTransfering;
+	local allowAttacking = Mod.Settings.allowAttacking;
+	local allowReconBox = Mod.Settings.allowReconBox;
+
+        if startWithCommander == nil then startWithCommander = true; end
+	if allowTransfering == nil then allowTransfering = true; end
+	if allowAttacking == nil then allowAttacking = false; end
+	if allowReconBox == nil then allowReconBox = true; end
+
+	startWithCommanderBox = UI.CreateCheckBox(vert).SetText('All starting territories receive a commander').SetIsChecked(startWithCommander);
+	allowTransferingBox = UI.CreateCheckBox(vert).SetText('Allows that a commander can transfer').SetIsChecked(allowTransfering);
+	allowAttackingBox = UI.CreateCheckBox(vert).SetText('Allows that a commander can attack/transfer').SetIsChecked(allowAttacking);
+	allowReconBox = UI.CreateCheckBox(vert).SetText('Reconnaissance cards spawn a commander on a territory').SetIsChecked(allowReconBox);
 end
